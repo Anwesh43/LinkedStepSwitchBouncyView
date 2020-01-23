4f88554ebc702f7a065cd68e37278853a119521d
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 10:24:06 +0530

    adding activity in view

--- app/src/main/java/com/anwesh/uiprojects/linkedstepswitchbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepswitchbouncyview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedstepswitchbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.stepswitchbouncyview.StepSwitchBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepSwitchBouncyView.create(this)
     }
 }
