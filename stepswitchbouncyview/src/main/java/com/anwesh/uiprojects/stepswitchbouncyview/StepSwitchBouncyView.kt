47346c0743b2913405956a8d7e3dc59e9bbfa75f
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 09:47:52 +0530

    adding Canvas extension methods

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -20,8 +20,46 @@ val delay : Long = 20
 val switchColor : Int = Color.parseColor("#3F51B5")
 val foreColor : Int = Color.parseColor("#757575")
 val backColor : Int = Color.parseColor("#FAFAFA")
+val rFactor : Float = 8f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.divideScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.maxScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepSwitch(i : Int, scale : Float, w : Float, paint : Paint) {
+    paint.color = switchColor
+    val gap : Float = w / (switches)
+    val sf : Float = scale.sinify().divideScale(i, switches)
+    val r : Float = gap / rFactor
+    save()
+    drawLine(0f, 0f, gap * sf, 0f, paint)
+    drawCircle(r + gap * sf, 0f, r, paint)
+    restore()
+}
+
+fun Canvas.drawStepSwitches(scale : Float, w : Float, paint : Paint) {
+    val scDiv : Double = 1.0 / switches
+    val i : Int = Math.floor(scale.toFloat() / scDiv).toInt()
+    for (j in 0..(i)) {
+        drawStepSwitch(j, scale, w, paint)
+    }
+}
+
+fun Canvas.drawFullLine(w : Float, paint : Paint) {
+    paint.color = foreColor
+    drawLine(0f, 0f, w, 0f, paint)
+}
+
+fun Canvas.drawSSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(0f, gap * (i + 1))
+    drawStepSwitches(scale, w, paint)
+    drawFullLine(w, paint)
+    restore()
+}
