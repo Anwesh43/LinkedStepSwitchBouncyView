21de4be58ec950deaf12405e472524b249a0c286
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 10:07:43 +0530

    adding StepSwitchBouncy class

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -170,4 +170,28 @@ class StepSwitchBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepSwitchBouncy(var i : Int) {
+
+        private val root : SSBNode = SSBNode(0)
+        private var curr : SSBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
