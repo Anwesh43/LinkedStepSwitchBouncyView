fdceeba03f66d7ec4af16c64e2732d6553b11750
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 09:31:17 +0530

    adding nodes and libraries

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.stepswitchbouncyview
 /**
  * Created by anweshmishra on 23/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val switches : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val switchColor : Int = Color.parseColor("#3F51B5")
+val foreColor : Int = Color.parseColor("#757575")
+val backColor : Int = Color.parseColor("#FAFAFA")
