113fab1c2bdfb58d897ce4c4f257e0796cab109e
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 09:32:45 +0530

    adding StepSwitchBouncyView class

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -20,3 +20,8 @@ val delay : Long = 20
 val switchColor : Int = Color.parseColor("#3F51B5")
 val foreColor : Int = Color.parseColor("#757575")
 val backColor : Int = Color.parseColor("#FAFAFA")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.divideScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.maxScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
