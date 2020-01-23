5b0cddf2d5b2661826641c812dea1e55242da747
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 09:49:28 +0530

    adding StepSwitchBouncyView class

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -63,3 +63,21 @@ fun Canvas.drawSSBNode(i : Int, scale : Float, paint : Paint) {
     drawFullLine(w, paint)
     restore()
 }
+
+class StepSwitchBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
