d0faf7a13d43c525cb1b7aab5fde9521de1d3370
Anwesh43 <anweshthecool0@gmail.com>
2020-01-23 10:04:15 +0530

    adding SSBNode class

--- stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
+++ stepswitchbouncyview/src/main/java/com/anwesh/uiprojects/stepswitchbouncyview/StepSwitchBouncyView.kt
@@ -128,4 +128,46 @@ class StepSwitchBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSBNode(var i : Int, val state : State = State()) {
+
+        private var next : SSBNode? = null
+        private var prev : SSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSBNode {
+            var curr : SSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
